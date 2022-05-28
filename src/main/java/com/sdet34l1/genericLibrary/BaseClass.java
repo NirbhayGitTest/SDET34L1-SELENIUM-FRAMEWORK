0db07ef0d6d4fd96fe172d0daf7586795be8e14a
Nirbhay The Omen <Nirbhay The Omen@LAPTOP-VTMNI8FB>
2022-05-28 17:11:46 +0530

    updated base class

--- src/main/java/com/sdet34l1/genericLibrary/BaseClass.java
+++ src/main/java/com/sdet34l1/genericLibrary/BaseClass.java
@@ -24,7 +24,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 /**
  * This class contains implementation of all basic configuration annotations
  * @author Nirbhay The Omen
- *this is a base class very important
+ *this is a base class and
  */
 
 public class BaseClass {
